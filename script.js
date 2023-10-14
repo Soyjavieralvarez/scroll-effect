02d79d28b1e8fb36ec62f5b449795c3a2e909543
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-10-14 21:05:37 +0200

    scroll effect test

---
+++ script.js
@@ -0,0 +1,24 @@
+import gsap from './gsap';
+import ScrollTrigger from './ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+console.clear();
+gsap.config({ trialWarn: false});
+gsap.registerPlugin(ScrollTrigger);
+gsap.to("#container", {
+    "--target": "0%",
+    ease: "none",
+    scrollTrigger: {
+        trigger: "#container",
+        markers: {
+            startColor: "yellow",
+            endColor: "#42a6e0",
+            fontSize: "14px"
+        },
+        start: "top top",
+        end: "+=1000",
+        pin: true,
+        scrub: 1
+    }
+});
